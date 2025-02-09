f6ba4c140bf894430ae42ed4856f932a3f2a2898
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2025-02-09 22:58:36 +0500

    Multiple shopping lits

--- shopping_list/api/serializers.py
+++ shopping_list/api/serializers.py
@@ -6,8 +6,13 @@ from shopping_list.models import ShoppingItem, ShoppingList
 class ShoppingItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShoppingItem
-        fields = ['id', 'name', 'purchased', "shopping_list"]
-        read_only_fields = ('id',)
+        fields = ['id', 'name', 'purchased']
+        read_only_fields = ('id', )
+
+    def create(self, validated_data, **kwargs):
+        print(kwargs)
+        validated_data['shopping_list_id'] = self.context['request'].parser_context['kwargs']['pk']
+        return super().create(validated_data)
 
 
 class ShoppingListSerializer(serializers.ModelSerializer):
--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from shopping_list.api.viewsets import ShoppingItemViewSet, ShoppingListViewSet
-
-router = routers.DefaultRouter()
-router.register("shopping-items", ShoppingItemViewSet, basename="shopping-items")
-router.register("shopping-list", ShoppingListViewSet, basename="shopping-list")
+from shopping_list.api.views import ListAddShoppingList, ShoppingListDetail, AddShoppingItem, ShoppingItemDetail
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('shopping-lists/', ListAddShoppingList.as_view(), name="all-shopping-lists"),
+    path('shopping-lists/<uuid:pk>/', ShoppingListDetail.as_view(), name='shopping-list-detail'),
+    path('shopping-lists/<uuid:pk>/shopping-items/', AddShoppingItem.as_view(), name='shopping-items'),
+    path('shopping-lists/<uuid:pk>/shopping-items/<uuid:item_pk>/', ShoppingItemDetail.as_view(),
+         name='shopping-items-detail'),
 ]
